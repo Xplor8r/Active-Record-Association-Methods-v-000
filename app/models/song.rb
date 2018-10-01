790374cefeac3b0f9afdc6d085ea2449766919d4
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-01 19:55:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    drake = Artist.find_or_create_by(artist: "Drake")
-    self.artist = drake
+    .find_or_create_by(name: "Drake")
+    self.artist
   end
 end
